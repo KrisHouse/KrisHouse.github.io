4bf4bb2edfb0d016bbd71a81e72b6bc1f7d0a994
KrisHouse <151697278+KrisHouse@users.noreply.github.com>
2025-01-27 17:47:20 +0800

    Add types of monsters

--- TCWA/content/mysteries.js
+++ TCWA/content/mysteries.js
@@ -19,6 +19,14 @@ module.exports.mysteries = [
         ]
     },
     {
+        id: "runeTypes",
+        chapter: 55,
+        title: "Rune Types",
+        summary: "The twelve Rune Types are: Miracle, Damage, Life, Time-Space, Support, Poison, Buff, Guard, Summon, Psyche, Element, and Knowledge.",
+        tags: ["Talent, worldbuilding"],
+        sections: []
+    },
+    {
         id: "levelups",
         chapter: 55,
         title: "Level Ups",
@@ -56,5 +64,97 @@ module.exports.mysteries = [
                 content: "An awakener's conviction has to aligned with their Talent to merge with the corresponding Rune Circuit and reach level 8."
             },
         ]
+    },
+    {
+        id: "monsterTypes",
+        chapter: 30,
+        title: "Types of Monsters",
+        summary: "There are six types of monsters: delusion monster, greed monster, wrath monster, pride monster, life monster, and death monster.\n\
+        This is according to the line Baili Yi left Gao Yang with: Delusion, greed, wrath, pride, life, and death are all meaningless. Life is short and nothing but a grand dream.",
+        tags: ["monsters, worldbuilding"],
+        sections: []
+    },
+    {
+        id: "delusionMonsters",
+        chapter: 30,
+        title: "Delusion Monsters",
+        summary: "Also known as wanderers. The plebs of the monsters. They make up the majority of the monster population and are generally harmless. They modify their own memory to stay ignorant of the truth of the Mist World, or justify what they witness by modifying their logics.",
+        tags: ["monsters, worldbuilding"],
+        sections: []
+    },
+    {
+        id: "wrathMonsters",
+        chapter: 30,
+        title: "Wrath Monsters",
+        summary: "Monsters that actively attack humans once waking up. Stronger than wanderers.",
+        tags: ["monsters, worldbuilding"],
+        sections: [
+            {
+                title: "Slaughterers",
+                chapter: 30,
+                content: "The most offensive ones. Many take the forms of lizardpeople, but there are some resembling wolves and other animals as well. They generally can't be reasoned with, and will go after the awakeners once triggered."
+            },
+            {
+                title: "Devourers",
+                chapter: 30,
+                content: "Attack awakeners actively too, and their tentacles are their trademark."
+            },
+            {
+                title: "Summoners",
+                chapter: 30,
+                content: "Seem to be the most special type of wrath monsters. They can summon their fellow monsters and trigger their transformation within a certain range, and they seem to retain their ability to think once transformed. Dragon thus have come up with three questions to ask the summoners before killing them."
+            }
+        ]
+    },
+    {
+        id: "greedMonsters",
+        chapter: 57,
+        title: "Greed Monsters",
+        summary: "As the alt name would suggest, they make humans their host and can turn into half-humans.",
+        tags: ["monsters, worldbuilding"],
+        sections: [
+            {
+                title: "Freeriders",
+                chapter: 57,
+                content: "Take over only a part of the human body and may coexist with the human. Monsters shooting deadly rays from its mouth seems to be a common form freeriders take."
+            },
+            {
+                title: "Overtakers",
+                chapter: 57,
+                content: "Take over the human entirely to form a different identity. Their common form seem to be humanoid but covered in violet scales that serve as armor with bone stings as their weapons."
+            }
+        ]
+    },
+    {
+        id: "prideMonsters",
+        chapter: 85,
+        title: "Pride Monsters",
+        summary: "There are three subtypes depending on their stance toward humans. They are much more powerful than wrath monsters and greed monsters, and they maintain a consistent identity rather than switching between their personhood and monsterhood like other monsters.",
+        tags: ["monsters, worldbuilding"],
+        sections: [
+            {
+                title: "Lightbringers",
+                chapter: 85,
+                content: "Said to actively help humans."
+            },
+            {
+                title: "Shadowstalkers",
+                chapter: 85,
+                content: "Actively hunt humans."
+            },
+            {
+                title: "Observers",
+                chapter: 85,
+                content: "Takes a neutral stance."
+            }
+        ]
+    },
+    {
+        id: "lifeMonsters",
+        chapter: 553,
+        title: "Life Monsters",
+        summary: "Give birth to Spectres, Malediction, and Divine Scion, also responsible for releasing the Crimson Tides.",
+        tags: ["monsters, worldbuilding"],
+        sections: []
     },
 ];
